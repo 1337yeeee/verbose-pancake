4fd1c42275fcb34d409880134903a18879bfdeec
Anatoly <none>
2022-05-23 19:53:08 +0300

    небольшие нововведения

---
+++ Vision/Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Vision.Controllers {
+	public class SearchController : Controller {
+
+		private readonly ILogger<HomeController> _logger;
+		private readonly DbContext _context;
+
+		public SearchController(ILogger<HomeController> logger, DbContext context) {
+			_logger = logger;
+			_context = context;
+		}
+
+	}
+}
