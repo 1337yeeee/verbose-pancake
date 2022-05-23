b516d3bb1e234fdff9cf12c907402f9a84d0bdfd
dodya13 <nemeevadyan@mail.ru>
2022-05-23 20:14:37 +0300

    добавил ввод баз данных

--- Vision/Models/ViewModel.cs
+++ Vision/Models/ViewModel.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 namespace Vision.Models {
 	public class ViewModel {
 
-		//public IEnumerable<Product> Products { get; set; }
+		public IEnumerable<Product> Products { get; set; }
 		public IEnumerable<Category> Categories { get; set; }
-		//public IEnumerable<Brand> Brands { get; set; }
+		public IEnumerable<Brand> Brands { get; set; }
 		public IEnumerable<Article> Articles { get; set; }
 		public IEnumerable<Author> Authors { get; set; }
+
 
 
 		//public ViewModel() {
