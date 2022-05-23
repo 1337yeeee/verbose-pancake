58130c84dae02359036fc67c604759740ed44795
dodya13 <nemeevadyan@mail.ru>
2022-05-23 22:10:44 +0300

    создал user

--- Vision/Models/User.cs
+++ Vision/Models/User.cs
@@ -1,6 +1,18 @@
-﻿namespace Vision.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vision.Models
 {
     public class User
     {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Sexgender { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+
+
+
     }
 }
