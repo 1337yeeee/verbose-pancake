f7350fd7e71465b83b8962ef89bf390c02490f41
dodya13 <nemeevadyan@mail.ru>
2022-05-24 17:02:51 +0300

    link

---
+++ Vision/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Vision.Models;
+
+namespace Vision.Controllers
+{
+    public class CategoryController: Controller
+    {
+        private readonly DbContext _context;
+        public CategoryController(DbContext context)
+        {
+            _context = context;
+        }
+        public ViewResult List()
+        {
+            //var clothes = _allClothes.Goods;
+            var categories = _context.Categories;
+            return View(categories);
+        }
+        [HttpGet]
+        [Route("/Category/Index/{id:guid}")]
+        public IActionResult Index(Guid id)
+        {
+            Category category = _context.Categories.Include(category => category.clothes).FirstOrDefault(brand => brand.id == id);
+            return View(category);
+        }
+    }
+}
